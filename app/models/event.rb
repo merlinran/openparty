1be39cb04a0e2bf8263e5599f10da02740363062
Merlin Ran <merlinran@163.com>
2013-03-30 00:38:39 +0000

    asdf

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,16 @@
 class Event < ActiveRecord::Base
   has_many :enrolments
+
+  def held?
+    self.hold_at < Date.today();
+  end
+
+  def self.past_events
+    self.where(held?: true)
+  end
+
+  def self.current_events
+    self.where(held?: false)
+  end
+
 end
