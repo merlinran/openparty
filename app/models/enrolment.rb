1a6ecc48a438afe3b896f3f89acf0d237d9d10a9
Merlin Ran <merlinran@163.com>
2013-04-06 15:14:04 +0000

    add validation to enrolment

--- app/models/enrolment.rb
+++ app/models/enrolment.rb
@@ -1,4 +1,7 @@
 class Enrolment < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
+
+  validates_presence_of :user_id, :event_id
+  validates_uniqueness_of :user_id, scope: :event_id
 end
